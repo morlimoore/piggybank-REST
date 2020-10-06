6622936bb6d5b88aab4feb0700766750b0c364d9
morlimoore <cooliyk2@gmail.com>
2020-10-06 23:29:38 +0100

    Bootstrap application

---
+++ src/main/java/com/morlimoore/piggybankapi/PiggybankApiApplication.java
@@ -0,0 +1,13 @@
+package com.morlimoore.piggybankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiggybankApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiggybankApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/morlimoore/piggybankapi/PiggybankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morlimoore.piggybankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiggybankApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
