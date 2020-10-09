fa9b5f4d119a912c5359fd871d2074cbd5aea3c9
morlimoore <cooliyk2@gmail.com>
2020-10-09 01:16:55 +0100

    Enable asynchronous processing

--- src/main/java/com/morlimoore/piggybankapi/PiggybankApiApplication.java
+++ src/main/java/com/morlimoore/piggybankapi/PiggybankApiApplication.java
@@ -1,7 +1,12 @@
 package com.morlimoore.piggybankapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableAsync
