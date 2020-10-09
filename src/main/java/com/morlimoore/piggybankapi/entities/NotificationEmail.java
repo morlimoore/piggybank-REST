08b9c413c7024305f4bee63c65090f05ab37f873
morlimoore <cooliyk2@gmail.com>
2020-10-09 01:14:12 +0100

    Implement NotificationEmail on signup

---
+++ src/main/java/com/morlimoore/piggybankapi/entities/NotificationEmail.java
@@ -0,0 +1,14 @@
+package com.morlimoore.piggybankapi.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationEmail {
+    private String subject;
+    private String recipient;
+    private String body;
+}
