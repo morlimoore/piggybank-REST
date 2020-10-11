8a00b036eae91c1fe380b2ec2a7c8607c8a557a0
morlimoore <cooliyk2@gmail.com>
2020-10-11 03:26:12 +0100

    Extract fields to BaseEntity

--- src/main/java/com/morlimoore/piggybankapi/entities/Token.java
+++ src/main/java/com/morlimoore/piggybankapi/entities/Token.java
@@ -3,22 +3,15 @@ package com.morlimoore.piggybankapi.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name="tokens")
-public class Token {
-
-    @Id
-    @Column(unique = true, nullable = false)
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
+public class Token extends BaseEntity {
 
     @Column(nullable = false)
     private String token;
@@ -26,12 +19,8 @@ public class Token {
     @Column(nullable = false, length = 25)
     private String purpose;
 
-    @Column(nullable = false)
-    private Boolean is_valid;
-
-    @CreationTimestamp
-    @Column(nullable = false)
-    private Timestamp created_at;
+    @Column(name = "is_valid", nullable = false)
+    private Boolean isValid;
 
     @ManyToOne
     @JoinColumn(name="user_id", nullable = false)
--- src/main/java/com/morlimoore/piggybankapi/entities/User.java
+++ src/main/java/com/morlimoore/piggybankapi/entities/User.java
@@ -3,32 +3,22 @@ package com.morlimoore.piggybankapi.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.sql.Date;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name="users")
-public class User {
+public class User extends BaseEntity {
 
-    @Id
-    @Column(unique = true, nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "first_name", nullable = false, length = 25)
+    private String firstName;
 
-    @Column(nullable = false, length = 25)
-    private String first_name;
-
-    @Column(nullable = false, length = 25)
-    private String last_name;
+    @Column(name = "last_name", nullable = false, length = 25)
+    private String lastName;
 
     @Column(unique = true, nullable = false, length = 40)
     private String email;
@@ -36,31 +26,27 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false, length = 25)
-    private String phone_number;
+    @Column(name = "phone_number", nullable = false, length = 25)
+    private String phoneNumber;
 
-    @Column(nullable = false)
-    private Date date_of_birth;
+    @Column(name = "date_of_birth", nullable = false)
+    private Date dateOfBirth;
 
-    @CreationTimestamp
-    @Column(nullable = false)
-    private Timestamp created_at;
-
-    @Column(nullable = false)
-    private Boolean is_active;
+    @Column(name = "is_enabled", nullable = false)
+    private Boolean isEnabled;
 
     @Column(nullable = false, length = 25)
     private String role;
 
-    public User(String first_name, String last_name,
-                String email, String password, Date date_of_birth,
-                Boolean is_active, String role) {
-        this.first_name = first_name;
-        this.last_name = last_name;
+    public User(String firstName, String lastName,
+                String email, String password, Date dateOfBirth,
+                Boolean isEnabled, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
-        this.date_of_birth = date_of_birth;
-        this.is_active = is_active;
+        this.dateOfBirth = dateOfBirth;
+        this.isEnabled = isEnabled;
         this.role = role;
     }
 
