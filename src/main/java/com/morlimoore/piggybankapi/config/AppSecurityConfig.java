016d8a90ab42684b8491bb53bbb701ed2cbeaac5
morlimoore <cooliyk2@gmail.com>
2020-10-11 03:15:54 +0100

    Update AppSecurityConfig

--- src/main/java/com/morlimoore/piggybankapi/config/AppSecurityConfig.java
+++ src/main/java/com/morlimoore/piggybankapi/config/AppSecurityConfig.java
@@ -1,19 +1,67 @@
 package com.morlimoore.piggybankapi.config;
 
+import com.morlimoore.piggybankapi.security.JwtAuthenticationEntryPoint;
+import com.morlimoore.piggybankapi.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtAuthenticationEntryPoint unauthorizedHandler;
+    private JwtTokenProvider tokenProvider;
+
+    public AppSecurityConfig(@Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService,
+                             JwtAuthenticationEntryPoint unauthorizedHandler,
+                             PasswordEncoder passwordEncoder,
+                             JwtTokenProvider tokenProvider) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+        this.unauthorizedHandler = unauthorizedHandler;
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/api/auth/**");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
-                .antMatchers("/api/auth/**")
-                .permitAll()
                 .anyRequest()
                 .authenticated();
+        http.apply(new JwtTokenFilterConfig(tokenProvider));
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
