76555bda6e23730d65b7462cd35f3f5cfe950d04
morlimoore <cooliyk2@gmail.com>
2020-10-11 03:26:12 +0100

    Extract fields to BaseEntity

--- src/main/java/com/morlimoore/piggybankapi/entities/Transaction.java
+++ src/main/java/com/morlimoore/piggybankapi/entities/Transaction.java
@@ -3,32 +3,21 @@ package com.morlimoore.piggybankapi.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name="transactions")
-public class Transaction {
-
-    @Id
-    @Column(unique = true, nullable = false)
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
+public class Transaction extends BaseEntity {
 
     @Column(nullable = false, length = 25)
     private String type;
 
     @Column(nullable = false)
     private Long amount;
-
-    @Column(nullable = false)
-    @CreationTimestamp
-    private Timestamp created_at;
 
     @Column(length = 100)
     private String remarks;
