9b8dd28475b4b6c5f9ea32e5443c90775e529c7b
morlimoore <cooliyk2@gmail.com>
2020-10-11 03:17:10 +0100

    Update AuthService

--- src/main/java/com/morlimoore/piggybankapi/service/AuthService.java
+++ src/main/java/com/morlimoore/piggybankapi/service/AuthService.java
@@ -1,10 +1,15 @@
 package com.morlimoore.piggybankapi.service;
 
+import com.morlimoore.piggybankapi.dto.LoginUserRequestDto;
 import com.morlimoore.piggybankapi.dto.RegisterUserRequestDto;
+import com.morlimoore.piggybankapi.payload.ApiResponse;
+import org.springframework.http.ResponseEntity;
 
 public interface AuthService {
 
-    void signup(RegisterUserRequestDto registerUserRequestDto);
+    ResponseEntity<ApiResponse<String>> signup(RegisterUserRequestDto registerUserRequestDto);
 
-    void verifyAccount(String token);
+    ResponseEntity<ApiResponse<String>> verifyAccount(String token);
+
+    ResponseEntity<Object> login(LoginUserRequestDto loginUserRequestDto);
 }
