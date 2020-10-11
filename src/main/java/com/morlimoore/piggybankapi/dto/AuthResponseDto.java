88602a96d3edc8a5c6964da488b255ed5cb7f682
morlimoore <cooliyk2@gmail.com>
2020-10-11 03:16:27 +0100

    Add AuthResponseDto

---
+++ src/main/java/com/morlimoore/piggybankapi/dto/AuthResponseDto.java
@@ -0,0 +1,14 @@
+package com.morlimoore.piggybankapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponseDto {
+
+    private String authenticationToken;
+    private String email;
+}
