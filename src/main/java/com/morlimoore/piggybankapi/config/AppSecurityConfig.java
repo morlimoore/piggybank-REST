978b1a6919152e892466de174784f440e14cb705
morlimoore <cooliyk2@gmail.com>
2020-10-16 12:15:11 +0100

    Updates ignored route due to updated servlet context path

--- src/main/java/com/morlimoore/piggybankapi/config/AppSecurityConfig.java
+++ src/main/java/com/morlimoore/piggybankapi/config/AppSecurityConfig.java
@@ -40,7 +40,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/api/auth/**");
+        web.ignoring().antMatchers("/auth/**");
     }
 
     @Override
