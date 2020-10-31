905cd915488df437380fbb6358a35b93debfc329
morlimoore <cooliyk2@gmail.com>
2020-10-31 13:42:03 +0100

    Adds bindingResultError

--- src/main/java/com/morlimoore/piggybankapi/util/CreateResponse.java
+++ src/main/java/com/morlimoore/piggybankapi/util/CreateResponse.java
@@ -1,12 +1,23 @@
 package com.morlimoore.piggybankapi.util;
 
 import com.morlimoore.piggybankapi.payload.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 
 public class CreateResponse {
 
     public static <T> ResponseEntity<ApiResponse<T>> createResponse(ApiResponse<T> response) {
         return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    public static ResponseEntity<ApiResponse<String>> bindingResultError(BindingResult result) {
+        ApiResponse<String> response = new ApiResponse<>(HttpStatus.BAD_REQUEST);
+        response.setMessage("Validation error");
+        response.setDebugMessage(result.getFieldError().getDefaultMessage());
+        response.setError(result.getFieldError().toString());
+        response.addValidationErrors(result.getFieldErrors());
+        return createResponse(response);
     }
 }
 
