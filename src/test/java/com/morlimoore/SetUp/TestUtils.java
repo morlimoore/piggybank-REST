97e6fa07b02745b7209c87ddfb7243d658051537
morlimoore <cooliyk2@gmail.com>
2021-01-28 10:34:20 +0100

    Removes call to JwtTokenProvider

--- src/test/java/com/morlimoore/SetUp/TestUtils.java
+++ src/test/java/com/morlimoore/SetUp/TestUtils.java
@@ -3,7 +3,6 @@ package com.morlimoore.SetUp;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.morlimoore.piggybankapi.security.JwtTokenProvider;
 
 public class TestUtils {
 
