a7d25f06d4f4c0e94f789c05b3ada6a7dd91df75
morlimoore <cooliyk2@gmail.com>
2021-02-01 09:45:56 +0100

    Removes UnirestException

--- src/main/java/com/morlimoore/piggybankapi/service/AuthService.java
+++ src/main/java/com/morlimoore/piggybankapi/service/AuthService.java
@@ -1,15 +1,13 @@
 package com.morlimoore.piggybankapi.service;
 
-import com.mashape.unirest.http.exceptions.UnirestException;
 import com.morlimoore.piggybankapi.dto.LoginUserRequestDTO;
 import com.morlimoore.piggybankapi.dto.RegisterUserRequestDTO;
 import com.morlimoore.piggybankapi.payload.ApiResponse;
-import com.morlimoore.piggybankapi.payload.JwtResponse;
 import org.springframework.http.ResponseEntity;
 
 public interface AuthService {
 
-    ResponseEntity<ApiResponse<String>> signup(RegisterUserRequestDTO registerUserRequestDto) throws UnirestException;
+    ResponseEntity<ApiResponse<String>> signup(RegisterUserRequestDTO registerUserRequestDto);
 
     ResponseEntity<?> login(LoginUserRequestDTO loginUserRequestDto);
 
