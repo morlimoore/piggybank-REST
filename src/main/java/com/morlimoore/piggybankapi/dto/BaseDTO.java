9e55d0fd1d68670b556ce8426411ee487bca8773
morlimoore <cooliyk2@gmail.com>
2021-02-01 09:48:23 +0100

    Adds validation

--- src/main/java/com/morlimoore/piggybankapi/dto/BaseDTO.java
+++ src/main/java/com/morlimoore/piggybankapi/dto/BaseDTO.java
@@ -4,13 +4,16 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public abstract class BaseDTO {
 
     @Email(message="Please enter a valid Email address")
+    @NotBlank(message = "Email cannot be blank")
     private String email;
 
     @NotBlank(message="Please enter a password")
+    @Size(min = 6, message = "Password cannot be less than six(6) characters")
     private String password;
 }
