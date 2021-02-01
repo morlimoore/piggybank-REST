48f8445efa9ab50a7f10f5a0cd954fb5af1c81a9
morlimoore <cooliyk2@gmail.com>
2021-02-01 09:52:54 +0100

    Removes UnirestException, adds logger and handles exception

--- src/main/java/com/morlimoore/piggybankapi/service/MailService.java
+++ src/main/java/com/morlimoore/piggybankapi/service/MailService.java
@@ -1,9 +1,7 @@
 package com.morlimoore.piggybankapi.service;
 
-import com.mashape.unirest.http.exceptions.UnirestException;
-
 public interface MailService {
 
-    void sendActivationMail(String email, String token) throws UnirestException;
+    void sendActivationMail(String email, String token);
 
 }
--- src/main/java/com/morlimoore/piggybankapi/service/impl/MailServiceImpl.java
+++ src/main/java/com/morlimoore/piggybankapi/service/impl/MailServiceImpl.java
@@ -1,13 +1,18 @@
 package com.morlimoore.piggybankapi.service.impl;
 
 import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
+import com.morlimoore.piggybankapi.exceptions.CustomException;
 import com.morlimoore.piggybankapi.service.MailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MailServiceImpl implements MailService {
+
+    private Logger logger = LoggerFactory.getLogger(MailServiceImpl.class);
 
     @Value("${application.mail-client.password}")
     private String password;
@@ -16,15 +21,20 @@ public class MailServiceImpl implements MailService {
     private String domain;
 
     @Override
-    public void sendActivationMail(String email, String token) throws UnirestException {
-        Unirest.post("https://api.mailgun.net/v3/" + domain + "/messages")
-            .basicAuth("api", password)
-            .queryString("from", "SAVE-SAFE Admin <admin@savesafe.com>")
-            .queryString("to", email)
-            .queryString("subject", "Welcome to Save Safe")
-            .queryString("text", "Thank you for signing up to the SAVE-SAFE App. \n" +
-                    "Please click on, or copy and paste the below URI in your browser, to activate your account: \n" +
-                    "https://localhost:8443/api/v1/auth/accountVerification/" + token)
-            .asJson();
+    public void sendActivationMail(String email, String token) {
+        try {
+            Unirest.post("https://api.mailgun.net/v3/" + domain + "/messages")
+                    .basicAuth("api", password)
+                    .queryString("from", "SAVE-SAFE Admin <admin@savesafe.com>")
+                    .queryString("to", email)
+                    .queryString("subject", "Welcome to Save Safe")
+                    .queryString("text", "Thank you for signing up to the SAVE-SAFE App. \n" +
+                            "Please click on, or copy and paste the below URI in your browser, to activate your account: \n" +
+                            "https://localhost:8443/api/v1/auth/accountVerification/" + token)
+                    .asJson();
+        } catch(Exception e) {
+            logger.error("Check that the email client password is correct.");
+            throw new CustomException("Please contact the administrator.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
